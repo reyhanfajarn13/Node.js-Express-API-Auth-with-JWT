0c22fcaa1fb4d4f4930c56c403640fe8c4046d87
reyhanfajarn13 <reyhanfajarn13@gmail.com>
2022-07-11 17:45:53 +0700

    adding bcrypt module for hashing the password

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const userModel = require('../models/user')
 const { registerValidation } = require('../routes/validation')
 
+// import the bcrypt module for encrypt the password
+const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req,res) => {
     //VALIDATE DATA BEFORE HAVE A USER
@@ -14,11 +16,15 @@ router.post('/register', async (req,res) => {
     const emailExist = await userModel.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exist');
 
+    //HASH THE PASSWORD 
+    const salt = await bcrypt.genSalt(10); // this code will be adding 10 random code to the password
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     //Create a new user
   const user = new userModel({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
    });
    
     try{
