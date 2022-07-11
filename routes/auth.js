6790532b3ea8c1cd6b4976a521e984f64705bb86
reyhanfajarn13 <reyhanfajarn13@gmail.com>
2022-07-11 17:28:16 +0700

    Route the Validation Schema on validation.js and adding checker of an exist Email on auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,28 +2,19 @@ const express = require('express')
 const router = express.Router()
 
 const userModel = require('../models/user')
+const { registerValidation } = require('../routes/validation')
 
-//VALIDATION
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-    name: Joi.string()
-        .min(6)
-        .required(),
-    email: Joi.string()
-        .min(6)
-        .required()
-        .email(),
-    password: Joi.string()
-        .min(6)
-        .required()
-})
 
 router.post('/register', async (req,res) => {
     //VALIDATE DATA BEFORE HAVE A USER
-    const {error} = schema.validate(req.body);
+    const { error }= registerValidation(req.body);
     if(error) return res.status(400).send(error.details[0].message);
+   
+    //Checking if the user is already in the database
+    const emailExist = await userModel.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exist');
 
+    //Create a new user
   const user = new userModel({
         name: req.body.name,
         email: req.body.email,
